7e3de5b2e67819aa46cac6444faa9d978dee1cb0
Rinat Gabidullin <rinat@gabidullin.com>
2018-09-09 22:11:45 +0300

    init

--- frontend/src/components/channels/Channel.js
+++ frontend/src/components/channels/Channel.js
@@ -1,26 +1,29 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
-class Channel extends Component {
-  onClick (e) {
+class Channel extends Component{
+  onClick(e){
     e.preventDefault();
     const {setChannel, channel} = this.props;
     setChannel(channel);
   }
   render(){
-  const {channel} = this.props;
-  return (
-    <li>
-      <a onClick={this.onClick.bind(this)}>
-        {channel.name}
-      </a>
-    </li>
-  )
+    const {channel, activeChannel} = this.props;
+    const active = channel === activeChannel ? 'active' : '';
+    return (
+      <li className={active}>
+        <a onClick={this.onClick.bind(this)}>
+          {channel.name}
+        </a>
+      </li>
+    )
   }
 }
 
 Channel.propTypes = {
-  channel: React.PropTypes.object.isRequired,
-  setChannel: React.PropTypes.func.isRequired
+  channel: PropTypes.object.isRequired,
+  setChannel: PropTypes.func.isRequired,
+  activeChannel: PropTypes.object.isRequired
 }
 
 export default Channel
